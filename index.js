9d3fc1b15830b607681d22fa6cd460db6536ed79
Nick Zana <me@nickzana.dev>
2022-01-29 14:43:01 -0500

    Add state initialization to onStart

--- index.js
+++ index.js
@@ -41,35 +41,10 @@ Array.prototype.random = function (arr) {
 /**
  * Runs on launch of the site to do initial setup
  */
-function onStart() {}
-
-document.getElementById("start").onclick = function() {
-        // new speech recognition object
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-    var recognition = new SpeechRecognition();
-    var SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-
-    // This runs when the speech recognition service starts
-    recognition.onstart = function() {
-        console.log("We are listening. Try speaking into the microphone.");
-    };
-    // This runs when the speech recognition service returns result
-    recognition.onresult = function(event) {
-        var transcript = event.results[0][0].transcript;
-        var confidence = event.results[0][0].confidence;
-    };
-
-    // start recognition
-    $('start').on('click', function(e) {
-        recognition.start();
-      });
+function onStart() {
+	// TODO: Get default language from browser
+	state = new State('en-US', 5);
 }
 
-document.getElementById("stop").onclick = function() {}
-
-$('stop').on('click', function(_) {
-	recognition.stop();
-  });
-
+/** Execute initialization code */
 onStart();
