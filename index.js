0ecce7eb36a087bf196426de3c220d8b9b985539
Nick Zana <me@nickzana.dev>
2022-01-28 23:36:11 -0500

    Create a state class to represent the program

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+var state;
+
+/** Class representing the state of the program */
+class State {
+	/**
+	 * Create a state for the program
+	 * @param {string} language - the language the user speaks
+	 * @param {number} difficulty - the difficulty of the questions
+	 */
+	constructor(language, difficulty) {
+		this.language = language;
+		this.difficulty = difficulty;
+		this.question = randomQuestion(language, difficulty);
+	}
+}
+
+/**
+ * Return a question to ask the user
+ * @param {string} language - language of the question to return
+ * @param {number} difficulty - difficulty of the question between 1 and 10
+ * @returns {string}
+ */
+function randomQuestion(language, difficulty) {
+	// TODO: Pull a quote from a database
+	var questions = [
+		 "What is your favorite animal?",
+		 "What is your favorite color?"
+	];
+	return questions.random();
+}
+
+/**
+ * Return a random element from an array
+ * @param {*[]} arr - The array to select an element from
+ * @returns {*}
+ */
+Array.prototype.random = function (arr) {
+	return arr[Math.floor(Math.random() * arr.length())];
+}
+
+/**
+ * Runs on launch of the site to do initial setup
+ */
+function onStart() {}
+
 document.getElementById("start").onclick = function() {
         // new speech recognition object
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
@@ -26,3 +71,5 @@ document.getElementById("stop").onclick = function() {}
 $('stop').on('click', function(_) {
 	recognition.stop();
   });
+
+onStart();
